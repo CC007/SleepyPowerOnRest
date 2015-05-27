03f159c6cdbf460b3793fdd7060e69ae6e14eddb
app@dizkartes.nl <app@dizkartes.nl>
2015-05-27 11:40:04 +0200

    Added plugwise sensorid and instanceid

---
+++ src/main/java/com/github/cc007/sleepypoweron/RestController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.cc007.sleepypoweron;
+
+/**
+ *
+ * @author Maarten
+ */
+public class RestController {
+    
+}
